f524f08e7c32edf29f6f38cc02636bea9c7293cd
Anders Borch <anders@cyborch.com>
2024-11-18 19:39:58 +0100

    Add publishing configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("java")
     id("org.jetbrains.kotlin.jvm").version("2.0.20")
+    id("maven-publish")
 }
 
 group = "org.starcoin.rsa"
@@ -15,8 +16,20 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter")
     testImplementation("junit:junit:4.12")
     implementation("com.google.guava:guava:20.0")
+    implementation(kotlin("stdlib"))
 }
 
 tasks.test {
     useJUnitPlatform()
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+            groupId = "org.starcoin.rsa"
+            artifactId = "rsa-accumulator"
+            version = "1.0.0"
+        }
+    }
 }
